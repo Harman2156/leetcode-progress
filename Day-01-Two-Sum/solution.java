91420674a51cec62e884276abe9c3cc1d8e12820
Harman2156 <garhiaa2332@gmail.com>
2026-01-09 18:42:51 +0530

    Update solution.java

--- Day-01-Two-Sum/solution.java
+++ Day-01-Two-Sum/solution.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int romanToInt(String s) {
+    HashMap<Character , Integer> mm = new HashMap<>();
+    if(s == null || s.length() == 0) return 0;
+    mm.put('I' , 1);
+    mm.put('V' , 5);
+    mm.put('X' , 10);
+    mm.put('L' , 50);
+    mm.put('C' , 100);
+    mm.put('D' , 500);
+    mm.put('M' , 1000);
 
+    int result = mm.get(s.charAt(s.length() - 1));
+    for(int i = s.length()-2;i>= 0;i--){
+        if(mm.get(s.charAt(i)) < mm.get(s.charAt(i+1))){
+        result = result - mm.get(s.charAt(i));
+        }else{
+            result = result + mm.get(s.charAt(i));
+        }
+    }
+
+    return result;
+    }
+}
